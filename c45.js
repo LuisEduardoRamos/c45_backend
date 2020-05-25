95372fd4e694553c3d93d08cbfc5a67756a1fee1
LuisEduardoRamos <luiseduardoramosg@gmail.com>
2020-05-24 19:41:31 -0700

    integracion y creacion de ruta

--- c45.js
+++ c45.js
@@ -1,45 +1,11 @@
 const csv = require('csvtojson');
 const _ = require('lodash');
 let iteracion = 0;
+let arbolArray = [];
 let iteracionArray = [];
 let iteracionObject = new Object();
 let entropiasArray = [];
 let relacionTemporal = null;
-
-
-function calcularEntropia(datos, clase){
-    let total = datos.length;
-    let tipos = _.uniqBy(datos, clase);
-    let grupos = _.groupBy(datos, clase);
-    let tiposArray = [];
-    let entropiaClase = 0;
-    //Extrae los datos de la clase, sin repetir
-    for (let i = 0; i < tipos.length; i++) {
-        let json =  tipos[i];
-        tiposArray.push(json[clase]);
-    }
-    //Se calcula la entropía iterando las opciones
-    for (let i = 0; i < tiposArray.length; i++) {
-        //grupos es un arreglo donde se encuentran todos los datos agrupados por las opciones de la clase
-        let cantidadTipos = grupos[tiposArray[i]].length;
-        let relacion = cantidadTipos / total;
-        entropiaClase += entropia(relacion);
-    }
-    return entropiaClase;
-}
-
-function entropia(relacion){
-    return ((relacion) * Math.log2(relacion) * -1);
-}
-
-
-function agruparDatos(datos){
-    //Se extren las cabeceras del csv, en este caso pasan a ser las llaves del objeto
-    let cabeceras = Object.keys(datos[0]);
-    return cabeceras.map((cabecera)=>({
-       [cabecera]: _.groupBy(datos, cabecera)
-    }));
-}
 
 function c45(dataset, claseDecision){
     iteracionObject = {};        
@@ -73,7 +39,6 @@ function c45(dataset, claseDecision){
             if(entropiaGo == 0) {
                 let hoja = sub[0];
                 iteracionObject.desicionFinal = hoja[claseDecision];
-                console.log('se termina');
                 return;
             }
         }
@@ -103,12 +68,48 @@ function c45(dataset, claseDecision){
 
 }
 
+function calcularEntropia(datos, clase){
+    let total = datos.length;
+    let tipos = _.uniqBy(datos, clase);
+    let grupos = _.groupBy(datos, clase);
+    let tiposArray = [];
+    let entropiaClase = 0;
+    //Extrae los datos de la clase, sin repetir
+    for (let i = 0; i < tipos.length; i++) {
+        let json =  tipos[i];
+        tiposArray.push(json[clase]);
+    }
+    //Se calcula la entropía iterando las opciones
+    for (let i = 0; i < tiposArray.length; i++) {
+        //grupos es un arreglo donde se encuentran todos los datos agrupados por las opciones de la clase
+        let cantidadTipos = grupos[tiposArray[i]].length;
+        let relacion = cantidadTipos / total;
+        entropiaClase += entropia(relacion);
+    }
+    return entropiaClase;
+}
+
+function entropia(relacion){
+    return ((relacion) * Math.log2(relacion) * -1);
+}
+
+function agruparDatos(datos){
+    //Se extren las cabeceras del csv, en este caso pasan a ser las llaves del objeto
+    let cabeceras = Object.keys(datos[0]);
+    return cabeceras.map((cabecera)=>({
+       [cabecera]: _.groupBy(datos, cabecera)
+    }));
+}
 
 function print(title, body){console.log(title+': '+body);}
 
-async function main(ruta, claseDecision){
+
+module.exports = async (json, claseDecision) => {
     //Se obtienen los datos del csv y se parsean a JSON
-    let json = await csv().fromFile(ruta);
     c45(json, claseDecision);
-    console.log(iteracionArray)
+    return iteracionArray;
 }
+
+//main('datos/datosArbol-3.csv','Decision');
+//main('datos/banco.csv','class');
+//main("datos/datosArbol-4.csv","play")
--- controller.js
+++ controller.js
@@ -5,15 +5,17 @@ let api = express.Router();
 let multipart = require('connect-multiparty');
 let multipartMiddleware = multipart();
 const csv = require('csvtojson');
-
+const c45 = require('./c45');
 api.post("/upload",multipartMiddleware, async function(req, res){
-    console.log(req.files)
+    console.log(req.files);
     let datos = await csv().fromFile(req.files.csv.path);
     res.status(200).send(datos);
 })
 
-api.get('/c45', async function(req, res){
-    console.log(req.body)
+api.post('/c45', async function(req, res){
+    console.log(req.body);
+    let response = await c45(req.body.json, req.body.decision);
+    res.status(200).send(response)
 })
 
 module.exports = api;
